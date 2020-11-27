aafceb804303a4c20d6a6b1ba850fa6290b075e6
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-11-27 10:54:41 +0300

    source code

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', (req, res) => {
+    // Read the variables sent via POST from our API
+    const {
+        sessionId,
+        serviceCode,
+        phoneNumber,
+        text,
+    } = req.body;
+
+    let response = '';
+
+    if (text == '') {
+        // This is the first request. Note how we start the response with CON
+        response = `CON What would you like to check
+        1. My account
+        2. My phone number`;
+    } else if ( text == '1') {
+        // Business logic for first level response
+        response = `CON Choose account information you want to view
+        1. Account number
+        2. Account balance`;
+    } else if ( text == '2') {
+        // Business logic for first level response
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your phone number is ${phoneNumber}`;
+    } else if ( text == '1*1') {
+        // This is a second level response where the user selected 1 in the first instance
+        const accountNumber = 'ACC100101';
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your account number is ${accountNumber}`;
+    } else if ( text == '1*2') {
+        // This is a second level response where the user selected 1 in the first instance
+        const balance = 'KES 10,000';
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your balance is ${balance}`;
+    }
+
+    // Send the response back to the API
+    res.set('Content-Type: text/plain');
+    res.send(response);
+});
